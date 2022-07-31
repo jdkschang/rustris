7f8659405cac2c20b8414426a1e4f64c6c081d8b
Jonathan D. Chang <jdkschang@apple.com>
2022-07-31 14:31:42 -0700

    🧱✨ Add User implementation
    - 🧱✨ Add `Direction` and `rotate` implementations
    - 🧱✨ Add `game_over` implementation
    - 🧱✨ Add `remove_line`, `remove_full_line`

--- src/tetris.rs
+++ src/tetris.rs
@@ -1,4 +1,12 @@
+use std::mem;
+use std::collections::HashSet;
 use crate::shape::{Pos, Shape};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Direction {
+    Left,
+    Right,
+}
 
 #[derive(Debug)]
 pub struct Tetris {
@@ -6,6 +14,7 @@ pub struct Tetris {
     height: i32,
     current_shape: Shape,
     fixed_shapes: Vec<Shape>,
+    game_over: bool,
 }
 
 impl Tetris {
@@ -15,18 +24,42 @@ impl Tetris {
             height: height as i32,
             current_shape: &Shape::new_random() + Pos((width as i32 - 1) / 2, 0),
             fixed_shapes: vec![],
+            game_over: false,
         }
     }
 
     pub fn is_out_of_bounds(&self, shape: &Shape) -> bool {
-        shape.positions().all(|pos| pos.0 >= 0 && pos.0 < self.width && pos.1 >= 0 && pos.1 < self.height)
+        !shape.iter_positions().all(|pos| pos.0 >= 0 && pos.0 < self.width && pos.1 >= 0 && pos.1 < self.height)
     }
 
     pub fn is_colliding(&self, shape: &Shape) -> bool {
         self.fixed_shapes.iter().any(|fixed_shape| fixed_shape.collides_with(shape))
     }
 
+    pub fn is_line_full(&self, y: i32) -> bool {
+        self.fixed_shapes.iter()
+            .flat_map(|shape| shape.iter_positions())
+            .filter(|pos| pos.1 == y)
+            .collect::<HashSet<_>>()
+            .len() as i32 == self.width
+    }
+
+    fn remove_line(&mut self, y: i32) {
+        for shape in self.fixed_shapes.iter_mut() {
+            shape.remove_line(y);
+        }
+    }
+
+    fn remove_full_lines(&mut self) {
+        for y in 0..self.height {
+            if self.is_line_full(y) {
+                self.remove_line(y)
+            }
+        }
+    }
+
     pub fn tick(&mut self) {
+        if self.game_over { return; }
         let translated_current_shape = &self.current_shape + Pos(0, 1);
 
         if self.is_out_of_bounds(&translated_current_shape) || self.is_colliding(&translated_current_shape) {
@@ -37,8 +70,36 @@ impl Tetris {
             );
 
             self.fixed_shapes.push(new_fixed_shape);
+            self.remove_full_lines();
+
+            if self.is_colliding(&self.current_shape) {
+                self.game_over = true;
+            }
         } else {
             self.current_shape = translated_current_shape;
+        }
+    }
+
+    pub fn shift_current_shape(&mut self, direction: Direction) {
+        if self.game_over { return; }
+        let translated_current_shape = &self.current_shape + match direction {
+            Direction::Left => Pos(-1, 0),
+            Direction::Right => Pos(1, 0),
+        };
+
+        if !self.is_out_of_bounds(&translated_current_shape)
+            && !self.is_colliding(&translated_current_shape) {
+            self.current_shape = translated_current_shape;
+        }
+    }
+
+    pub fn rotate(&mut self) {
+        if self.game_over { return; }
+        let rotated_current_shape = self.current_shape.rotated();
+
+        if !self.is_out_of_bounds(&rotated_current_shape)
+            && !self.is_colliding(&rotated_current_shape) {
+            self.current_shape = rotated_current_shape;
         }
     }
 }
